6999b05b9b030039cb11dc5b25466f9c721cb699
vuphuctho <vuphuctho93@gmail.com>
2013-08-28 10:35:48 +0800

    add read ignore words

--- src/Input.java
+++ src/Input.java
@@ -16,7 +16,7 @@ public class Input {
 			if (line.compareTo("")!= 0) {
 				storage.addLine(line);
 			} else {
-				System.out.println("Do you want to stop? y/n: ");
+				System.out.print("Do you want to stop? y/n: ");
 				line = sc.nextLine();
 				if (line.toLowerCase().compareTo("y")==0) {
 					break;
@@ -26,6 +26,27 @@ public class Input {
 					System.out.println("Please enter lines:");
 				}
 			}
+		}
+
+		System.out.println();
+		System.out.println("Please enter words to ignore: ");
+		
+		while (true) {
+			String line = sc.nextLine();
+			if (line.compareTo("")!=0) {
+				String[] words = line.split("\\s+");
+				for (int i=0; i<words.length; i++) {
+					storage.addIgnoredWord(words[i]);
+				}
+			} else {
+				System.out.print("Do you want to stop? y/n: ");
+				line = sc.nextLine();
+				if (line.toLowerCase().compareTo("y")==0) {
+					break;
+				} else {
+					System.out.println("Please enter words to ignore:");
+				}
+			} 
 		}
 	}
 }
